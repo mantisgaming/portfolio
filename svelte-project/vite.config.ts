4c124edd68dd7be14515607946ac46e8c26fc70a
Max Allen <mjbgtaad56@gmail.com>
2025-12-30 18:50:29 -0500

    Organized svelte project

--- svelte-project/vite.config.ts
+++ svelte-project/vite.config.ts
@@ -4,4 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "./",
+  appType: "spa",
+  build: {
+    cssMinify: true
+  },
+  root: "./site"
 })
