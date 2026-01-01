39db94b571b89fc81750d4a89a1a9b24c216e995
Max Allen <mjbgtaad56@gmail.com>
2026-01-01 09:16:47 -0500

    Updated portrait and blurb

--- svelte-project/site/src/App.svelte
+++ svelte-project/site/src/App.svelte
@@ -8,12 +8,13 @@
   <Router {url}>
     <Route path="/">
       <div class="jumbotron">
-        <img alt="Self Portrait" src={portrait} />
+        <img class="portrait" alt="Self Portrait" src={portrait} />
         <div>
           <h1 class="no-select">Max Allen</h1>
           <strong class="no-select">They / Them &mdash; Technical Game Developer</strong>
           <p>Hello and welcome to my portfolio website! I am a game developer specializing in the technical aspects of game development. Some of my specialties are software architecture, online networking, and graphics programming.</p>
-          <p>Scroll down to view a collection of my projects which highlight my skills.</p>
+          <p>Scroll down to view a collection of my projects which highlight my skills!</p>
+          <p>For any business inquiries, feel free to email me at <a href="mailto:maxwell.allen.business@gmail.com" target="_blank">maxwell.allen.business@gmail.com</a></p>
         </div>
       </div>
     </Route>
@@ -21,6 +22,11 @@
 </main>
 
 <style>
+  .portrait {
+    border-radius: 2rem;
+    box-shadow: 0 0 3rem -1rem black;
+  }
+
   .jumbotron {
     display: flex;
     flex-direction: row;
@@ -35,7 +41,6 @@
     margin: 0;
 
     &::after {
-      transition: 0.25s ease-out;
       content: " ";
       display: block;
       width: 70%;
@@ -44,11 +49,6 @@
       margin: 0% 15%;
       border-radius: 1px;
       transform: translateY(-3px);
-    }
-
-    &:hover::after {
-      width: 90%;
-      margin: 0% 5%;
     }
   }
 </style>
