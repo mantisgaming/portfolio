37b71b05349778075f5e7d667db0749b1b85deb5
Max Allen <mjbgtaad56@gmail.com>
2026-07-02 08:27:38 -0400

    Set up wrangler

---
+++ src/lib/server/db.ts
@@ -0,0 +1,5 @@
+import type { ProjectData } from "$lib/types/projectData";
+
+export async function getProjectFromDatabase(slug: string): Promise<ProjectData> {
+    return {};
+}
---
+++ src/routes/project/[slug]/+page.server.ts
@@ -0,0 +1,15 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { getProjectFromDatabase } from '$lib/server/db';
+
+export const prerender = false;
+
+export const load: PageServerLoad = async ({ params }) => {
+    const project = await getProjectFromDatabase(params.slug);
+
+    if (project) {
+        return project;
+    }
+
+    error(404, 'Not found');
+}
