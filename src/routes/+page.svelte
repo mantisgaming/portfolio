6c43e9ac84d3abf3090a70c7ee79591ece0e5a5d
Max Allen <mjbgtaad56@gmail.com>
2026-07-02 08:36:04 -0400

    edited home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Max Allen</h1>
+<p>Technical Game Programmer</p>
+<p>Portfolio construction in progress...</p>
